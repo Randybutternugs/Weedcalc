445776c57a7ae4d396e6bf77137ee02011953093
randybutternugs <adonisalexis@icloud.com>
2020-08-19 23:27:15 -0400

    Functioning, conv data to dicts and add pickl

--- Weed.py
+++ Weed.py
@@ -1,5 +1,9 @@
 #%%
+import os
+import shutil
+import pickle
 from flask import Flask, request
+from flask import render_template
 from twilio.twiml.messaging_response import Message, MessagingResponse
 from twilio.rest import Client
 import sys
@@ -8,6 +12,8 @@ from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+app = Flask(__name__)
 
 account_sid = 'ACdf779c87eb69f16a893c436695c4779f'
 auth_token = '7301f66dcd1ed9db550f1e1434fa2274'
@@ -69,16 +75,63 @@ def test():
 	date.extend([fir, sec, thi, fou, fif])
 	cash.extend([firc, secc, thic, fouc, fifc])
 
-app = Flask(__name__)
+
+def graph():
+	plt.figure(figsize=(15,15))
+	plt.plot_date(datefin, cashfin, marker='o', linestyle = '-', color='mediumvioletred')
+	plt.savefig('chart.png')
+
+def move():
+	shutil.move('chart.png', 'static\chart.png')
+
+
+@app.route("/", methods=['GET'])
+def hello():
+	return(render_template('index.html'))
 
 @app.route('/sms', methods=['POST'])
 def sms():
-	number = "My Lovely Makayla"
-	message_body = request.form['Body']
-
+	incoming_msg = request.values.get('Body').lower()
 	resp = MessagingResponse()
-	resp.message('Hello {}, you said: {}'.format(number, message_body))
+	msg = resp.message()
+	responded = False
+
+	if 'show' in incoming_msg:
+		msg.body("Available")
+		msg.media('http://ad4b7cb2ae5a.ngrok.io/static/chart.png')
+		responded = True
+		return str(resp)
+
+	if '/' in incoming_msg:
+		msg.body("Date Recorded. ")
+		date.append(incoming_msg)
+		return str(resp)
+
+	if '190' in incoming_msg:
+		msg.body("Amount Recorded. ")
+		cash.append(incoming_msg)
+		return str(resp)
+	
+	if 'add' in incoming_msg:
+		msg.body("All set!")
+		dater(date)
+		numberer(cash)
+		graph()
+		return str(resp)
+
+	if 'push' in incoming_msg:
+		msg.body("Changes Saved! ")
+		move()
+		return str(resp)
+
+
+	if not responded:
+		pa = 'Not '
+		rt = 'Available'
+		msg.body(pa + rt)
+	
 	return str(resp)
+
 
 if __name__ == '__main__':
 	app.run()
@@ -106,20 +159,13 @@ print(cash)
 print(datefin)
 print(cashfin)
 
-plt.figure(figsize=(15,15))
+plt.figure(figsize=(10,10))
 plt.plot_date(datefin, cashfin, marker='o', linestyle = '-', color='mediumvioletred')
-plt.show()
+plt.savefig('chart.png')
 
 
 
  # %%
-message = client.messages \
-    .create(
-         body='I love Makayla',
-         from_='+19362182353',
-         to='+17277440419'
-     )
-
-print(message.sid)
-
+print(date)
+print(cash)
 # %%
